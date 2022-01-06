2c8173035fbc5f2837881cbaa78e9f87d1d10f90
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2022-01-06 15:07:50 -0300

    widget teste'

---
+++ test/dashboard_widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('should display the main image when the dashboard is opended',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mainImage = find.byType(Image);
+    expect(mainImage, findsOneWidget);
+    //findsNothing
+  });
+}
